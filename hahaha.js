53e104a751b818e6bfc4e13a66369f42d7dedbb3
klarastrinic <45819882+klarastrinic@users.noreply.github.com>
2020-04-14 19:42:38 +0200

    Add files via upload

---
+++ hahaha.js
@@ -0,0 +1,17 @@
+$("#leafers").on("click", function(event) {
+    // Do something!
+    console.log("leafer clicked");
+    $("#leafers").attr("src", "mjordan.jpg");
+});
+
+$("#beach").on("click", function(event) {
+    // Do something!
+    console.log("beach clicked");
+    $("#beach").attr("src", "curry.jpg");
+});
+
+$("#hopper").on("click", function(event) {
+    // Do something!
+    console.log("beach clicked");
+    $("#hopper").attr("src", "james.jpg");
+});
